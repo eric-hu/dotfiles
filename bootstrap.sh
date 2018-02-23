8f333dd7fbc8d4891aa005334437c68ada77b50a
Eric Hu <erichu@Eric-CCI-MBP.local>
2018-02-23 17:10:44 +0700

    Add Vundle install to setup check

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,13 @@
 DIRECTORY=$(cd `dirname $0` && pwd)
 
 # keep things minimal until I need to expand this
+echo "linking .vimrc"
 DOTFILE=$DIRECTORY"/.vimrc"
 [ -f "$DOTFILE" ] && ln -sv "$DOTFILE" ~
+
+echo "installing Vundle if not installed"
+# Install vundler for vim and macvim
+VUNDLE_HOME=~/.vim/bundle/Vundle.vim
+[ ! -d $VUNDLE_HOME ] &&
+  git clone https://github.com/VundleVim/Vundle.vim.git $VUNDLE_HOME
 
