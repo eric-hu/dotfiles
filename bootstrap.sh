4a6d4d813e310e4b8d40f3f684114e8ef256bd18
Eric Hu <hu.eric@gmail.com>
2018-02-25 22:08:18 +0700

    Add a few more dotfiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,36 @@
 
 DIRECTORY=$(cd `dirname $0` && pwd)
 
+echo "assuming $DIRECTORY for current file path"
+
 # keep things minimal until I need to expand this
 DOTFILE=$DIRECTORY"/.vimrc"
+
+# Only link dotfile if it exists
+# Do not overwrite original dotfile
+[ -f "$DOTFILE" ] && ln -sv "$DOTFILE" ~
+
+DOTFILE=$DIRECTORY"/.bash_profile"
+
+# Only link dotfile if it exists
+# Do not overwrite original dotfile
+[ -f "$DOTFILE" ] && ln -sv "$DOTFILE" ~
+
+DOTFILE=$DIRECTORY"/.gitconfig"
+
+# Only link dotfile if it exists
+# Do not overwrite original dotfile
+[ -f "$DOTFILE" ] && ln -sv "$DOTFILE" ~
+
+DOTFILE=$DIRECTORY"/.gitignore-global"
+
+# Only link dotfile if it exists
+# Do not overwrite original dotfile
+[ -f "$DOTFILE" ] && ln -sv "$DOTFILE" ~
+
+DOTFILE=$DIRECTORY"/.inputrc"
+
+# Only link dotfile if it exists
+# Do not overwrite original dotfile
 [ -f "$DOTFILE" ] && ln -sv "$DOTFILE" ~
 
